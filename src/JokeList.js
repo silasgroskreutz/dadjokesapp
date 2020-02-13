67aaa7819c90f75d9963f52e9bebc20895ed3019
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-13 14:08:19 -0600

    Added upvote/downvote feature

---
+++ src/JokeList.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Joke from './Joke';
+import uuid from 'uuid/v4';
+import './JokeList.css';
+
+class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+
+  async componentDidMount() {
+    //Load jokes from API
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get('https://icanhazdadjoke.com', {
+        headers: { Accept: 'application/json' }
+      });
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
+    }
+    this.setState({ jokes: jokes });
+  }
+
+  handleVote(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    }));
+  }
+  render() {
+    return (
+      <div className='JokeList'>
+        <div className='jokeList-sidebar'>
+          <h1 className='jokeList-title'>
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
+            alt='laughing face'
+          />
+          <button className='JokeList-getmore'>New Jokes</button>
+        </div>
+
+        <div className='JokeList-jokes'>
+          {this.state.jokes.map(j => (
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote={() => this.handleVote(j.id, 1)}
+              downvote={() => this.handleVote(j.id, -1)}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default JokeList;
